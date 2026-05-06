884703e5db8a007b9dfa2ccc300c36e77f2c6bc2
DTunjano <dylan2566@gmail.com>
2026-05-06 00:21:16 -0500

    configuracion inicial del repo + instalacion de dependencias.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://localhost:5173'],
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+  console.log(`Server is running on http://localhost:${port}`);
 }
 bootstrap();
